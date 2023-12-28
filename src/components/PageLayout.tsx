f262379470ec819c459f8f31122539e3d8c968ab
Adeboye Osomo <boyeosomo@gmail.com>
2023-12-28 13:26:58 +0000

    updated node modules, react-script and typescipt compatibility

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -12,10 +12,10 @@ interface PageLayoutProps {
 const PageLayout: React.FC<PageLayoutProps> = ({ title, detail, date, children }) => {
     return (
         <div>
-            <Typography variant="h2" component="h2" gutterBottom>
+            <Typography variant="h3" component="h3" gutterBottom>
                 {title}
             </Typography>
-            <Typography variant="h4" component="h4" gutterBottom>
+            <Typography variant="h6" component="h6" gutterBottom>
                 {detail}
             </Typography>
             <Typography variant="h4" component="h4" gutterBottom>
